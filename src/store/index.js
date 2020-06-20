b40ef1133fd5d5081054aac0c784495fbea0b4fd
lucasflaquer <lucas.flaquer@crbconstrutora.com.br>
2020-06-19 23:00:56 -0300

    projeto inicial

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: {
+      email:'lucas.flaquer@gmail.com',
+      password:'123456',
+      name: 'Lucas Henrique',
+      token:'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1OTI0NDQ2NDUsIm5iZiI6MTU5MjQ0NDY0NSwianRpIjoiOGQ1OGI2MTAtYWUwYS00YWQwLTk1NGYtZjA3OTMzYjNmMjcyIiwiZXhwIjoxNTkyODc2NjQ1LCJpZGVudGl0eSI6IjVlZTk3NGViY2UxYTg3NjE2ZGZkZmRkYiIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.1_iQpb4HRqGevPYqb2Sp8XDqPBxsQ7qdPbu-Xsgmaw4'
+    }
   },
   mutations: {
   },
