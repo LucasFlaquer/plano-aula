fa0cb9634bfc13e1645dc8316d8ef632e4d33bd8
lucasflaquer <lucas.flaquer@crbconstrutora.com.br>
2020-07-15 09:34:34 -0300

    lint

---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+// my preference
+module.exports = {
+  semi: false,
+  singleQuote: true
+}
---
+++ src/views/AddCurso.vue
@@ -0,0 +1,19 @@
+<template>
+  <Dashboard>
+    <!--  -->
+  </Dashboard>
+</template>
+
+<script>
+import Dashboard from '@/components/Dashboard'
+export default {
+  name: 'AddCurso',
+  components: {
+    Dashboard
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
